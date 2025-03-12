e8be3e2d3f2ee8593be47a0deb36ad5a5d9c99f1
Blocky060 <166016583+Blocky060@users.noreply.github.com>
2025-03-12 13:59:15 +0900

    finish

--- test.py
+++ test.py
@@ -3,11 +3,18 @@ b = 20
 c = 30
 d = 40
 
-print (a + b)
-print (c - d)
+#print (a + b)
+#print (c - d)
 
-print("test done")
-print("test done")
+#print("test done")
+#print(len("testdone"))
 #SecondTest
 
+input_a = float(input("첫 번째 숫자 : "))
+input_b = float(input("두 번째 숫자 : "))
+
+print("덧셈 결과 : ", input_a + input_b)
+print("빨셈 결과 : ", input_a - input_b)
+print("곱셈 결과 : ", input_a * input_b)
+print("나눗셈 결과 : ", input_a / input_b)
 
