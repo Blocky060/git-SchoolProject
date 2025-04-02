0369b5366ee1e12ea7347e847753fd01dbfda080
Blocky060 <166016583+Blocky060@users.noreply.github.com>
2025-04-02 13:22:19 +0900

    fix

--- 수업5회차/과제_calculate_X.py
+++ 수업5회차/과제_calculate_X.py
@@ -5,5 +5,5 @@ print(line)
 for i in range(1,10):
     line = ""
     for k in range(2,10):
-        line += (" %dX %d= %d" % (k, i, k*i))
+        line += ("%dX %d= %d " % (k, i, k*i))
     print(line)
