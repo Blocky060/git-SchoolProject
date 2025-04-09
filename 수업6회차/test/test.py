11c4882eef8dcf603032af9d74b3c242150760dd
Blocky060 <166016583+Blocky060@users.noreply.github.com>
2025-04-09 12:36:19 +0900

    def

--- 수업6회차/test/test.py
+++ 수업6회차/test/test.py
@@ -1,2 +1,30 @@
 #test
 print("testing")
+
+def get_shop_name() :
+    return ("커피장인")
+
+def get_branch_name() :
+    return ("여의도 본점")
+
+def print_name() :
+    print(get_shop_name())
+    print(get_branch_name())
+
+print_name()
+
+a = [1, 2, 3]
+a.insert(1, 4)
+print(a)
+
+order_detail = []
+
+def make_order (name, qty) :
+    order_detail.append({"이름" : name, "수량" : qty})
+
+print(order_detail)
+make_order("아메리카노", 2)
+make_order("카페라떼", 4)
+print(order_detail)                                                                                                                                                                                                                                                                                                                                                                                                                                               
+
+#빈칸채우기, 결과예상, 코드체우기 등 / 12시 10분
