94842f38be7331a50eb4ca9abb1ffb0698029728
Blocky060 <166016583+Blocky060@users.noreply.github.com>
2025-04-16 12:29:53 +0900

    fix

---
+++ 수업7회차/test/test.py
@@ -0,0 +1,38 @@
+def para_func (*para) :
+    result = 0
+    for I in para :
+        result += I
+    return result
+
+
+hap = para_func(10, 20, 30)
+print(hap)
+
+
+def dic_func (**para) :
+    for k in para.keys() :
+        print(k, "-->", para[k], "명입니다.")
+
+dic_func(트와이스 = 9, 소녀시대 = 7, 걸스데이 = 4, 블랙핑크 = 4)
+
+import random
+
+def get() :
+    return random.randrange(1, 46)
+lotto = []
+num = 0
+
+print("** 로또 추첨을 시작합니다. **\n")
+
+while True :
+    num = get()
+    if (lotto.count(num) == 0) :
+        lotto.append(num)
+    if (len(lotto) >= 6) :
+        break
+print("추첨된 로또 번호 ==> ", end = '')
+lotto.sort() 
+for i in range(6) :
+    print("%d " %lotto[i],end = '')
+
+
